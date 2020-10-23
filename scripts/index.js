110bc1deb4f4f6a9e7ce752298bbf434691395f0
samueledellavedova <46026355+samueledellavedova@users.noreply.github.com>
2020-10-23 10:32:25 +0200

    Bug Fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,7 @@
-const splash = $('#splash');
-
-const startbutton = $('#startbutton')
-const startmenu = $('#startmenu');
-
-const icons = $('.icons');
-const selection = $('#selection');
 var firstX, firstY, down = false;
 
 const notHoveringButtons = () => {
-  return !startbutton.is(':hover');
+  return !$('#startbutton').is(':hover');
 }
 const notHoveringIcons = () => {
   return $('.icons.focus:hover').length === 0;
@@ -16,7 +9,7 @@ const notHoveringIcons = () => {
 
 function boot() {
   const message = 'Welcome to my Odd World...';
-  const callback = () => setTimeout(() => splash.fadeOut(1000), 500);
+  const callback = () => setTimeout(() => $('#splash').fadeOut(1000), 500);
   
   write('#splashmessage', message, 0, 105, callback)
 }
@@ -32,62 +25,70 @@ function write(target, message, index, interval, callback) {
 
 // Handle start-button click
 
-startbutton.on('click', function() {
-  return alert('Ooops! I\'m sorry, the website\'s currently under construction :)');
-  startmenu.toggleClass('open');
+$('#startbutton').on('click', function() {
+  $('#startmenu').toggleClass('open');
+
+  if ($(this).hasClass('button-off'))
+    $(this).removeClass('button-off').addClass('button-on');
+  else 
+    $(this).removeClass('button-on').addClass('button-off');
 });
 
 // Handle single, double and right clicks on icons
 
-icons.on('click', function() {
-  icons.removeClass('focus').removeClass('border');
+$('.icons').on('click', function() {
+  $('.icons').removeClass('focus').removeClass('border');
   $(this).addClass('focus').addClass('border');
 
 }).on('dblclick', function() {
-  return alert('Ooops! I\'m sorry, the website\'s currently under construction :)');
+  // TODO double click
 
 }).on('contextmenu', function() {
-  return alert('Ooops! I\'m sorry, the website\'s currently under construction :)');
+  // TODO right click menu
 });
 
 // Pressing in any part of the screen (that's not over a button) will remove all hover effects and will hide start menu
 
-$(document).on('click', function() {
-  if (notHoveringIcons()) icons.removeClass('focus');
-
-  if (notHoveringButtons() && startmenu.hasClass('open')) startmenu.removeClass('open');
-});
-
-$(document).mousedown(function(e) {
-  selection.remove();
+$(document).on('mousedown', function(e) {
+  $("#selection").remove();
   down = true;
   firstX = e.clientX;
   firstY = e.clientY;
-}).mousemove(function(e) {
+
+}).on('mousemove', function(e) {
   if (down) {
-    $('body').append("<div id='selection'></div>");
-    selection.css('top',e.clientY+'px');
-    selection.css('left',e.clientX+'px');
+    $("body").append('<div id="selection"></div>');
+    $("#selection").css("top",e.clientY+"px");
+    $("#selection").css("left",e.clientX+"px");
     var diffX = e.clientX-firstX,
         diffY = e.clientY-firstY;
     if (diffY<=0) {
-      selection.css('top',e.clientY+'px');
+      $("#selection").css("top",e.clientY+"px");
     } else {
-      selection.css('top',firstY+'px');
+      $("#selection").css("top",firstY+"px");
     }
     if (diffX<=0) {
-      selection.css('left',e.clientX+'px');
+      $("#selection").css("left",e.clientX+"px");
     } else {
-      selection.css('left',firstX+'px');
+      $("#selection").css("left",firstX+"px");
     }
-    selection.css('width',Math.abs(diffX)+'px');
-    selection.css('height',Math.abs(diffY)+'px');
-    $('.icons').removeClass('focus border');
-    selection.overlaps('.icons').addClass('focus border');
+    $("#selection").css("width",Math.abs(diffX)+"px");
+    $("#selection").css("height",Math.abs(diffY)+"px");
+    $(".icons").removeClass("focus border");
+    $("#selection").overlaps(".icons").addClass("focus border");
   } else {
-    selection.remove();
+    $("#selection").remove();
   }
-}).mouseup(function() {
-  selection.remove();
+
+}).on('mouseup', function() {
+  $("#selection").remove();
   down = false;
-})
+
+}).on('click', function() {
+  if (notHoveringIcons()) $('.icons').removeClass('focus');
+
+  if (notHoveringButtons() && $('#startmenu').hasClass('open')) {
+    $('#startmenu').removeClass('open');
+    $('#startbutton').removeClass('button-on').addClass('button-off');
+  }
+});
